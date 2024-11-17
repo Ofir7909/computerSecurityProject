3bd226e94914ac87f832abe7450822ab24c6aabd
Ofir Lachmi <ofir7909@gmail.com>
2024-11-17 11:03:11 +0200

    addded index, login, register screens

---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def login(request):
+    return HttpResponse("login screen.")
+
+
+def register(request):
+    return HttpResponse("register screen.")
