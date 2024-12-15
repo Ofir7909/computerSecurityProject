94b7c229f109d721350d9fe6b69a0605f98b6740
Ofir Lachmi <ofir7909@gmail.com>
2024-12-15 11:18:06 +0200

    Upgraded Register page

--- website/forms.py
+++ website/forms.py
@@ -6,3 +6,13 @@ class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100)
     password = forms.CharField(
         label="Password", max_length=100, widget=forms.PasswordInput())
+
+
+class RegisterForm(forms.Form):
+    # TODO: change max_length with config file
+    username = forms.CharField(
+        label="Username", min_length=3, max_length=100, required=True)
+    email = forms.EmailField(
+        label="Email", max_length=100, required=True)
+    password = forms.CharField(
+        label="Password", max_length=100, widget=forms.PasswordInput(), required=True)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
+from django.db import IntegrityError
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
-from .forms import LoginForm
+from django.http import HttpResponse, HttpRequest
+from .forms import LoginForm, RegisterForm
+from .models import User
 
 
 def index(request):
@@ -20,9 +22,26 @@ def login(request):
         return HttpResponse(form.data)
 
 
-def register(request):
-    template = loader.get_template("register.html")
-    return HttpResponse(template.render(request=request))
+def register(request: HttpRequest):
+    register_template = loader.get_template("register.html")
+    if request.method == "GET":
+        form = RegisterForm()
+        return HttpResponse(register_template.render({"form": form}, request))
+
+    elif request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if not form.is_valid():
+            return HttpResponse(register_template.render({"form": form}, request))
+
+        try:
+            user = User.objects.create_user(
+                form.data["username"], form.data["email"], form.data["password"])
+        except IntegrityError:
+            form.add_error(None, "User already exists")
+            return HttpResponse(register_template.render({"form": form}, request))
+
+        return HttpResponse("user created successfully")
 
 
 def reset_password(request):
