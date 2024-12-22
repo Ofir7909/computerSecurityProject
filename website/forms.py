fc9574777c1b0ff5318b24ffd26bf3f3bb36941a
Ofir7909 <38537328+Ofir7909@users.noreply.github.com>
2024-12-22 10:47:30 +0200

    fix merge conflict at forms.py

--- website/forms.py
+++ website/forms.py
@@ -6,7 +6,7 @@ class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100)
     password = forms.CharField(
         label="Password", max_length=100, widget=forms.PasswordInput())
-<<<<<<< HEAD
+
     
 class ForgotPasswordForm(forms.Form):
     email = forms.EmailField(label="Enter your email address", required=True)
@@ -31,10 +31,6 @@ class ResetPasswordForm(forms.Form):
         widget=forms.PasswordInput()
     )
 
-
-=======
-
-
 class RegisterForm(forms.Form):
     # TODO: change max_length with config file
     username = forms.CharField(
@@ -43,4 +39,3 @@ class RegisterForm(forms.Form):
         label="Email", max_length=100, required=True)
     password = forms.CharField(
         label="Password", max_length=100, widget=forms.PasswordInput(), required=True)
->>>>>>> 94b7c229f109d721350d9fe6b69a0605f98b6740
