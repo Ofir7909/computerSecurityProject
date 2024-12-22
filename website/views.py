d9f5d0c1926a7f0711c37d80c81c9e4a7fbe9bab
SmoFF <ah_roi@walla.co.il>
2024-12-22 11:13:30 +0200

    login update

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .forms import LoginForm
+from .models import User
 
 
 def index(request):
@@ -15,6 +16,17 @@ def login(request):
         form = LoginForm()
         return HttpResponse(template.render({"form": form}, request))
     elif request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None: #https://stackoverflow.com/questions/16853044/logging-an-abstract-user-in
+            return render(request, 'login.html', 
+                {'message': 'Login Successfully'})
+        else:
+            return render(request, 'login.html', 
+                {'message': 'Bad username or password'})
+    else:
+        #return render(request, 'login.html')
         form = LoginForm(request.POST)
         print(form.data)
         return HttpResponse(form.data)
@@ -28,3 +40,14 @@ def register(request):
 def reset_password(request):
     template = loader.get_template("reset_password.html")
     return HttpResponse(template.render(request=request))
+
+def authenticate(self, username=None, password=None):
+        try:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+
+    
