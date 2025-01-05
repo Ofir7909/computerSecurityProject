51a9ecd081ebdadeb0ca084173f4a9908156ff9f
Ofir Lachmi <ofir7909@gmail.com>
2025-01-05 10:32:38 +0200

    added client model

--- website/models.py
+++ website/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
 class User(AbstractUser):
     username = models.CharField(max_length=64, unique=True)
     email = models.EmailField()
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(default=timezone.now)
