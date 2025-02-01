80f0076d197218bc02ccc483d3db256fb6576ca2
Ofir Lachmi <ofir7909@gmail.com>
2025-02-01 17:55:06 +0200

    removed old comments

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Token, User, Client, PasswordHistory
 
-# Register your models here.
 admin.site.register(User)
 admin.site.register(Client)
 admin.site.register(Token)
